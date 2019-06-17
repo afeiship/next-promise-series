e6dbc6e73dcc383b5de39de7e8dc0229698e1e3e
afeiship <1290657123@qq.com>
2019-06-17 11:04:06 +0800

    fix: number => const

--- dist/next-promise-series.js
+++ dist/next-promise-series.js
@@ -9,9 +9,10 @@
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var MSG_ERR = 'promise.series only accepts an array of functions';
+  var NUM = 'number';
 
   nx.promiseSeries = function(inTasks, inInitial) {
-    if (!typeof inTasks.length === 'number') {
+    if (!typeof inTasks.length === NUM) {
       return Promise.reject(new TypeError(MSG_ERR));
     }
     return inTasks.reduce(function(current, next) {
